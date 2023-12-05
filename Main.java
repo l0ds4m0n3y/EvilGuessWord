59f4119a365bf645b79e24f71a38d7915c8fd7cd
l0ds4m0n3y <dluna777@outlook.com>
2023-12-04 21:44:20 -0800

    it almost works but it might break

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -9,21 +9,22 @@ import java.util.Set;
 
 public class Main{
     static Map<Integer, Set<String>> wordLengthMap = new HashMap<>();
+    static Map<Integer, Set<String>> mapOfIndecies = new HashMap<>();
     static String gameString = "_";
-    static ArrayList<String> dictionary;
+    static int wordLength = 0;
+    static Set<String> mainSet = null;
+    static String triedWords;
+    static int triesLeft = 0;
 
     public static void main(String[] args) {
-        try { setUp(0); 
-        } catch (FileNotFoundException e) {}
+        try { setUp(0); } catch (FileNotFoundException e) {}
 
         char again = 'y';
         Scanner scan = new Scanner(System.in);
-        int triesLeft = 0;
-        int wordLength = 0;
-        Set<String> mainSet = null;
         
         //play again loop
         while(again == 'y'){
+            triedWords = "";
             while(mainSet == null || mainSet.size() == 0 || triesLeft <= 0){
                 System.out.print("Enter word Length: ");
                 wordLength = scan.nextInt();
@@ -34,28 +35,33 @@ public class Main{
                 triesLeft = scan.nextInt();
                 scan.nextLine(); //buffer
             }
-            mainSet.forEach(System.out::println);
             //clearConsole();
-
+            
             setUpBoard(wordLength);
-
+            
             // main game loop
             while(gameString.contains("_")){
                 if(triesLeft <= 0){
+                    clearConsole();
+                    System.out.println("LOSER");
+                    System.out.println(mainSet.iterator().next() + " was the final answer");
+                    System.out.println("types y to continue");
                     break;
                 }
-
+                clearConsole();
+                mainSet.forEach(System.out::println);
                 System.out.println(gameString);
-
-                doEvilStuff(scan.nextLine().charAt(0));
+                System.out.println(triedWords);
+                
+                mainSet = doEvilStuff(scan.nextLine().charAt(0));
             }
-            again = scan.nextLine().charAt(0);
+            again = scan.nextLine().toLowerCase().charAt(0);
         }   
         scan.close(); 
     }
 
     private static void setUp(int i) throws FileNotFoundException {
-        for(int j = 0; j < 30; j++ ){
+        for(int j = 0; j < 30; j++){
             wordLengthMap.put(j, new HashSet<String>());
         }
 
@@ -75,9 +81,27 @@ public class Main{
         fileScan.close();
     }
     
-    private static void doEvilStuff(char guess) {
-        
-    }
+    private static Set<String> doEvilStuff(char guess) {
+        for(int i = -1; i < wordLength; i++){
+            mapOfIndecies.put(i, new HashSet<>());
+        }
+        for(String s : mainSet){
+            mapOfIndecies.get(s.indexOf(guess)).add(s);
+            mapOfIndecies.get(s.lastIndexOf(guess)).add(s);
+        }
+
+        Set<String> testSet = new HashSet<>();
+        for(Map.Entry<Integer, Set<String>> set : mapOfIndecies.entrySet()){
+            if(testSet.size() < set.getValue().size()){
+                testSet = set.getValue();
+            }
+        }
+        if(testSet.equals(mapOfIndecies.get(-1))){
+            triedWords += guess + " ";
+            triesLeft--;
+        }
+        return testSet;
+    }   
     
     private static void setUpBoard(int length){
         for(int i = 1; i < length; i++){
